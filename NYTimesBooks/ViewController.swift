7833ff0026fc76fadb04d5853633e0b0c2cd2459
Alex Mefisto <alexandr.mefisto@gmail.com>
2023-02-04 18:55:05 +0200

    Initial Commit

---
+++ NYTimesBooks/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NYTimesBooks
+//
+//  Created by Alexandr Mefisto on 04.02.2023.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        
+    }
+
+
+}
+
