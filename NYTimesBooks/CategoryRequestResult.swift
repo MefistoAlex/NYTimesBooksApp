d3c5fcb0ec84931118c0534e14ea5cea09dfff4c
Alex Mefisto <alexandr.mefisto@gmail.com>
2023-02-04 20:24:48 +0200

    Create CategoryRequestResult.swift

---
+++ NYTimesBooks/CategoryRequestResult.swift
@@ -0,0 +1,25 @@
+//
+//  CategoryRequestResult.swift
+//  NYTimesBooks
+//
+//  Created by Alexandr Mefisto on 04.02.2023.
+//
+
+import Foundation
+struct CategoryRequestResult: Decodable {
+    let status: String
+    let num_results: Int
+    let results: [CategoryIncoming]
+
+    struct CategoryIncoming: Decodable {
+        let name: String
+        let nameEncoded: String
+        let newestPublishedDate: String
+
+        enum CodingKeys: String, CodingKey {
+            case name = "display_name"
+            case nameEncoded = "list_name_encoded"
+            case newestPublishedDate = "newest_published_date"
+        }
+    }
+}
