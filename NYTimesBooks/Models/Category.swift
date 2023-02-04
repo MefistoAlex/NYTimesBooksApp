9d9568044f528f95bb7a4efc1620a7d2db7dcd59
Alex Mefisto <alexandr.mefisto@gmail.com>
2023-02-04 20:44:37 +0200

    added Category

---
+++ NYTimesBooks/Models/Category.swift
@@ -0,0 +1,35 @@
+//
+//  Category.swift
+//  NYTimesBooks
+//
+//  Created by Alexandr Mefisto on 04.02.2023.
+//
+
+import Foundation
+struct Category: Hashable {
+    let name: String
+    let nameEncoded: String
+    let newestPublishedDate: String
+
+    init(from incomingCategory: CategoryRequestResult.CategoryIncoming) {
+        name = incomingCategory.name
+        nameEncoded = incomingCategory.nameEncoded
+        newestPublishedDate = incomingCategory.newestPublishedDate
+    }
+
+    init?(from entityCathegory: CategoryEntity) {
+        guard let name = entityCathegory.name,
+              let nameEncoded = entityCathegory.nameEncoded,
+              let newestPublishedDate = entityCathegory.newestPublishedDate else { return nil }
+
+        self.name = name
+        self.nameEncoded = nameEncoded
+        self.newestPublishedDate = newestPublishedDate
+    }
+
+    init(name: String, nameEncoded: String, newestPublishedDate: String) {
+        self.name = name
+        self.nameEncoded = nameEncoded
+        self.newestPublishedDate = newestPublishedDate
+    }
+}
