4f5802d46ecc67a225f10524a5e02329fc6cc5d4
Alex Mefisto <alexandr.mefisto@gmail.com>
2023-02-04 19:27:42 +0200

    added AlamofireAPIManager

---
+++ NYTimesBooks/AlamofireAPIManager.swift
@@ -0,0 +1,30 @@
+//
+//  AlamofireAPIManager.swift
+//  NYTimesBooks
+//
+//  Created by Alexandr Mefisto on 04.02.2023.
+//
+
+import Alamofire
+import Foundation
+final class AlamofireAPIManager: APIManager {
+    func request<T>(urlString: String,
+                    method: HttpMethod,
+                    dataType: T.Type,
+                    headers: [String: String]?,
+                    parameters: Parameters?,
+                    completion: @escaping (T?, Error?) -> Void) where T: Decodable {
+        var httpHeaders: HTTPHeaders?
+        if let headers = headers {
+            httpHeaders = HTTPHeaders(headers)
+        }
+
+        AF.request(urlString, method: HTTPMethod(rawValue: method.rawValue),
+                   parameters: parameters,
+                   headers: httpHeaders)
+            .validate()
+            .responseDecodable(of: T.self) { response in
+                completion(response.value, response.error)
+            }
+    }
+}
