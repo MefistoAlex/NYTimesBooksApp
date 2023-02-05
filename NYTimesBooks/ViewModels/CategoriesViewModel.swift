2ba72a8cc27b72ab1b5516b0b52637026f758c38
Alex Mefisto <alexandr.mefisto@gmail.com>
2023-02-05 17:50:12 +0200

    added CategoriesViewModel

---
+++ NYTimesBooks/ViewModels/CategoriesViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  CategoriesViewModel.swift
+//  NYTimesBooks
+//
+//  Created by Alexandr Mefisto on 05.02.2023.
+//
+
+import Foundation
+import RxSwift
+
+class CategoriesViewModel {
+    private let categoriesService: CategoriesAPIServiceProtocol
+    var categoriesError: Observable<Error> { errorSubject }
+    private let errorSubject = PublishSubject<Error>()
+
+    var categories: Observable<[Category]> { categoriesSubject }
+    private let categoriesSubject = BehaviorSubject(value: [Category]())
+
+    init() {
+        categoriesService = CategoriesAPIService()
+    }
+
+    func getNews() {
+        categoriesService.getCategories { [weak self] categories, error in
+            if let error {
+                self?.errorSubject.onNext(error)
+            }
+            if let categories {
+                self?.categoriesSubject.onNext(categories)
+            }
+        }
+    }
+}
