46ae09402bdea7c503dda4906859fca0ca247340
Alex Mefisto <alexandr.mefisto@gmail.com>
2023-02-05 10:41:40 +0200

    added init from incomingBook

--- NYTimesBooks/Models/Book.swift
+++ NYTimesBooks/Models/Book.swift
@@ -14,4 +14,25 @@ struct Book {
     let isnb13: String
     let amazonUrl: String
     let rank: Int
+
+    init(from incomingBook: BookRequestResult.BookIncoming) {
+        title = incomingBook.bookDetail[0].description
+        description = incomingBook.bookDetail[0].description
+        author = incomingBook.bookDetail[0].author
+        publisher = incomingBook.bookDetail[0].publisher
+        isnb13 = incomingBook.bookDetail[0].isnb13
+        amazonUrl = incomingBook.amazonUrl
+        rank = incomingBook.rank
+    }
+    
+
+    init(title: String, description: String, author: String, publisher: String, isnb13: String, amazonUrl: String, rank: Int) {
+        self.title = title
+        self.description = description
+        self.author = author
+        self.publisher = publisher
+        self.isnb13 = isnb13
+        self.amazonUrl = amazonUrl
+        self.rank = rank
+    }
 }
