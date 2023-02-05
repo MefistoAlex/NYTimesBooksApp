94e5ba4b79e9bb23d56f972e52999bd2b47924c0
Alex Mefisto <alexandr.mefisto@gmail.com>
2023-02-05 09:39:22 +0200

    added BookRequestResult and Book

---
+++ NYTimesBooks/Models/Book.swift
@@ -0,0 +1,17 @@
+//
+//  Book.swift
+//  NYTimesBooks
+//
+//  Created by Alexandr Mefisto on 05.02.2023.
+//
+
+import Foundation
+struct Book {
+    let title: String
+    let description: String
+    let author: String
+    let publisher: String
+    let isnb13: String
+    let amazonUrl: String
+    let rank: Int
+}
---
+++ NYTimesBooks/Models/BookRequestResult.swift
@@ -0,0 +1,42 @@
+//
+//  BookRequestResult.swift
+//  NYTimesBooks
+//
+//  Created by Alexandr Mefisto on 04.02.2023.
+//
+import Foundation
+struct BookRequestResult: Decodable {
+    let status: String
+    let num_results: Int
+    let results: [BookIncoming]
+
+    struct BookIncoming: Decodable {
+        let amazonUrl: String
+        let rank: Int
+        let bookDetail: [String]
+
+        enum CodingKeys: String, CodingKey {
+            case amazonUrl = "amazon_product_url"
+            case rank = "rank"
+            case bookDetail = "book_details"
+        }
+
+        struct BookDetail: Decodable {
+            let title: String
+            let description: String
+            let author: String
+            let publisher: String
+            let isnb13: String
+            
+            enum CodingKeys: String, CodingKey {
+                case title = "title"
+                case description = "description"
+                case author = "author"
+                case publisher = "publisher"
+                case isnb13 = "primary_isbn13"
+            }
+        }
+    }
+    
+
+}
