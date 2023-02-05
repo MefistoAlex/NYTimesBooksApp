6c4d044ecb297dcec035619436804e6a50f44254
Alex Mefisto <alexandr.mefisto@gmail.com>
2023-02-05 10:40:25 +0200

    Added Constants

---
+++ NYTimesBooks/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  NYTimesBooks
+//
+//  Created by Alexandr Mefisto on 05.02.2023.
+//
+
+import Foundation
+struct Constants {
+    static let apiKey = ["api-key": "sxjwEizy3R3Flcl5LMe6AYkEf9wDpPST"]
+    static let categoriesURL = "https://api.nytimes.com/svc/books/v3/lists/names.json"
+    static let booksURL = "https://api.nytimes.com/svc/books/v3/lists.json"
+}
