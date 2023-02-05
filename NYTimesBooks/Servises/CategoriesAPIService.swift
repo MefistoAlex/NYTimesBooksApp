fc0c9db9a57b38d0bf955686e9d1632138fbde0a
Alex Mefisto <alexandr.mefisto@gmail.com>
2023-02-05 10:45:21 +0200

    Create CategoriesAPIService.swift

---
+++ NYTimesBooks/Servises/CategoriesAPIService.swift
@@ -0,0 +1,41 @@
+//
+//  CategoriesAPIService.swift
+//  NYTimesBooks
+//
+//  Created by Alexandr Mefisto on 05.02.2023.
+//
+
+import Foundation
+
+protocol CategoriesAPIServiceProtocol {
+    func getCategories(completion: @escaping (_ articles: [Category]?, _ error: Error?) -> Void)
+}
+
+class CategoriesAPIService: CategoriesAPIServiceProtocol {
+    private let apiManager: APIManager
+
+    init() {
+        apiManager = AlamofireAPIManager()
+    }
+
+    func getCategories(completion: @escaping ([Category]?, Error?) -> Void) {
+        apiManager.request(urlString: Constants.categoriesURL,
+                           method: .get,
+                           dataType: CategoryRequestResult.self,
+                           headers: nil,
+                           parameters: Constants.apiKey) { data, error in
+            var categories: [Category]?
+            if let data {
+                categories = []
+                data.results.forEach {
+                    categories?.append(Category(from: $0))
+                }
+                completion(categories, nil)
+            }
+
+            if let error {
+                completion(nil, error)
+            }
+        }
+    }
+}
