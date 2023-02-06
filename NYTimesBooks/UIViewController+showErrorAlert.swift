b5e383fd112199f3ff316e7e6be80d716fc4e88a
Alex Mefisto <alexandr.mefisto@gmail.com>
2023-02-06 20:39:34 +0200

    added UIViewController+showErrorAlert

---
+++ NYTimesBooks/UIViewController+showErrorAlert.swift
@@ -0,0 +1,14 @@
+//
+//  UIViewController+showErrorAlert.swift
+//  NYTimesBooks
+//
+//  Created by Alexandr Mefisto on 06.02.2023.
+//
+import UIKit
+extension UIViewController {
+    func showErrorAlert(with error: Error) {
+        let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: "Default action"), style: .destructive))
+        present(alert, animated: true)
+    }
+}
