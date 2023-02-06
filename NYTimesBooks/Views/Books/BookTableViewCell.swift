bcc867aacabbc49284c59605ccb758a417c78635
Alex Mefisto <alexandr.mefisto@gmail.com>
2023-02-06 21:02:06 +0200

    created BooksTableViewController and BookTableViewCell

---
+++ NYTimesBooks/Views/Books/BookTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BookTableViewCell.swift
+//  NYTimesBooks
+//
+//  Created by Alexandr Mefisto on 06.02.2023.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
