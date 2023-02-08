2e0927456e796fcbb17255451b1d18449e67e757
Alex Mefisto <alexandr.mefisto@gmail.com>
2023-02-08 10:10:14 +0200

    created BookImageRequestResult

---
+++ NYTimesBooks/Models/APIModels/BookImageRequestResult.swift
@@ -0,0 +1,25 @@
+//
+//  BookImageRequestResult.swift
+//  NYTimesBooks
+//
+//  Created by Alexandr Mefisto on 08.02.2023.
+//
+
+import Foundation
+struct BookImageRequestResult : Decodable {
+    let items: [BookImageItem]?
+    
+    struct BookImageItem: Decodable {
+        let volumeInfo: VolumeInfo
+    }
+    
+    struct VolumeInfo: Decodable {
+        let imageLinks: ImageLink
+    }
+    struct ImageLink: Decodable {
+        let link: String
+        enum CodingKeys: String, CodingKey {
+            case link = "thumbnail"
+        }
+    }
+}
